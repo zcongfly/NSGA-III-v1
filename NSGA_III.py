7285d7995884aee7c0568714165db921b5a11d21
zcongfly <zcongflying@qq.com>
2023-10-21 09:08:49 +0800

    Add Annotations

--- NSGA_III.py
+++ NSGA_III.py
@@ -9,22 +9,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
-from itertools import combinations
-from scipy.linalg import LinAlgError
-from scipy.spatial.distance import cdist
+from itertools import combinations  # 创建和操作迭代器的工具
+from scipy.linalg import LinAlgError    # 用于处理线性代数相关的错误
+from scipy.spatial.distance import cdist    # 用于计算距离或相似性
 
 
 def cal_obj(pop, nobj):
-    # DTLZ1
-    g = 100 * (pop.shape[1] - nobj + 1 + np.sum((pop[:, nobj - 1:] - 0.5) ** 2 - np.cos(20 * np.pi * (pop[:, nobj - 1:] - 0.5)), axis=1))
-    objs = np.zeros((pop.shape[0], nobj))
-    temp_pop = pop[:, : nobj - 1]
-    for i in range(nobj):
-        f = 0.5 * (1 + g)
-        f *= np.prod(temp_pop[:, : temp_pop.shape[1] - i], axis=1)
+    '''
+    :param pop: ndarray，决策变量的取值，形状为(N, D)，N为种群中个体的数量，D为决策变量的数量
+    :param nobj: int，目标函数的数量
+    :return: ndarray，计算得到的目标函数值，形状为(N, nobj)，N是种群中个体的数量，nobj是目标函数的数量。
+    '''
+    # 这里的函数是 DTLZ1 函数，用于多目标优化问题。g 是一个中间变量。
+    g = 100 * (pop.shape[1] - nobj + 1 + np.sum(
+        (pop[:, nobj - 1:] - 0.5) ** 2 - np.cos(20 * np.pi * (pop[:, nobj - 1:] - 0.5)), axis=1))
+
+    objs = np.zeros((pop.shape[0], nobj))   # 创建一个大小为 (pop.shape[0], nobj) 的零矩阵，用于存储目标函数值。
+
+    temp_pop = pop[:, : nobj - 1]   # 从输入的 pop 矩阵中取出前 nobj-1 列，存储在 temp_pop 中。
+
+    for i in range(nobj):   # 遍历目标函数的数量（nobj）
+        f = 0.5 * (1 + g)   # 计算 f，其中 g 是前面计算的中间变量。
+
+        f *= np.prod(temp_pop[:, : temp_pop.shape[1] - i], axis=1)  # 使用累积乘法计算 f 的一部分，这部分与 temp_pop 相关。
+
         if i > 0:
-            f *= 1 - temp_pop[:, temp_pop.shape[1] - i]
-        objs[:, i] = f
+            f *= 1 - temp_pop[:, temp_pop.shape[1] - i]     # 如果 i 大于 0，再乘以 1 减去 temp_pop 的一部分。
+
+        objs[:, i] = f  # 将计算得到的 f 存储在目标函数矩阵的第 i 列。
+
     return objs
 
 
@@ -38,6 +51,7 @@ def factorial(n):
 
 def combination(n, m):
     # choose m elements from an n-length set
+    # 计算排列组合数C(n,m)
     if m == 0 or m == n:
         return 1
     elif m > n:
@@ -47,26 +61,41 @@ def combination(n, m):
 
 
 def reference_points(npop, nvar):
-    # calculate approximately npop uniformly distributed reference points on nvar dimensions
-    h1 = 0
-    while combination(h1 + nvar, nvar - 1) <= npop:
+    '''
+    calculate approximately npop uniformly distributed reference points on nvar dimensions
+    :param npop: int，要生成的均匀分布的参考点的数量
+    :param nvar: int，参考点的维度
+    :return: ndarray，所生成的均匀分布的参考点的坐标，形状为(npop, nvar)
+    '''
+    h1 = 0  # 用于控制循环的计数器
+    while combination(h1 + nvar, nvar - 1) <= npop: # 目的是确定一个足够大的h1，以便在nvar-1维度上有足够的均匀分布的参考点。
         h1 += 1
+    # 使用组合数的计算结果，构建一个 points 数组，这个数组包含了 h1 维度中的参考点坐标
     points = np.array(list(combinations(np.arange(1, h1 + nvar), nvar - 1))) - np.arange(nvar - 1) - 1
+    # 对 points 数组中的坐标进行变换，以确保它们在 [0, 1] 范围内均匀分布
     points = (np.concatenate((points, np.zeros((points.shape[0], 1)) + h1), axis=1) - np.concatenate((np.zeros((points.shape[0], 1)), points), axis=1)) / h1
-    if h1 < nvar:
+    if h1 < nvar:   # 如果h1不足以在nvar-1维度上生成足够多的参考点
         h2 = 0
+        # h2 的值，以便在nvar-1维度上有足够多的均匀分布的参考点。这些参考点将与之前的points组合在一起。
         while combination(h1 + nvar - 1, nvar - 1) + combination(h2 + nvar, nvar - 1) <= npop:
             h2 += 1
         if h2 > 0:
+            # 使用上边类似的方式，构建temp_points数组，然后进行坐标变换
             temp_points = np.array(list(combinations(np.arange(1, h2 + nvar), nvar - 1))) - np.arange(nvar - 1) - 1
             temp_points = (np.concatenate((temp_points, np.zeros((temp_points.shape[0], 1)) + h2), axis=1) - np.concatenate((np.zeros((temp_points.shape[0], 1)), temp_points), axis=1)) / h2
             temp_points = temp_points / 2 + 1 / (2 * nvar)
+            # 将temp_points添加到points中，以得到最终的参考点数组，并将其返回
             points = np.concatenate((points, temp_points), axis=0)
     return points
 
 
 def nd_sort(objs):
-    # fast non-domination sort
+    """
+    fast non-domination sort
+    :param objs: ndarray，种群中每个个体的目标函数值，形状为(npop, nobj)，其中npop是种群中个体的数量，nobj是目标函数的数量。
+    :return pfs: dict，键表示非支配级别，对应的值是一个包含相应级别的Pareto前沿中的个体索引的列表。
+    :return rank: ndarray，每个个体的非支配级别，形状为 (npop,)，其中npop是种群中个体的数量。rank数组指示了每个个体所属的非支配级别
+    """
     (npop, nobj) = objs.shape
     n = np.zeros(npop, dtype=int)  # the number of individuals that dominate this individual
     s = []  # the index of individuals that dominated by this individual
@@ -106,7 +135,14 @@ def nd_sort(objs):
 
 
 def selection(pop, pc, rank, k=2):
-    # binary tournament selection
+    """
+    binary tournament selection
+    :param pop: ndarray，种群中每个个体的决策变量值，形状为(npop,nvar)，即（种群中个体数量，决策变量数量）
+    :param pc: float，选择概率
+    :param rank: ndarray，每个个体的非支配级别，形状为(npop,)，即（种群中个体数，）
+    :param k: 锦标赛选择中参与竞争的个体数量
+    :return: ndarray，选择后得到的用于繁殖的个体的决策变量值，形状为(nm,nvar)，nm为选择个体数量，通常等于npop*pc，确保为偶数
+    """
     (npop, nvar) = pop.shape
     nm = int(npop * pc)
     nm = nm if nm % 2 == 0 else nm + 1
@@ -121,10 +157,18 @@ def selection(pop, pc, rank, k=2):
 
 
 def crossover(mating_pool, lb, ub, pc, eta_c):
-    # simulated binary crossover (SBX)
+    """
+    simulated binary crossover (SBX) 模拟二进制交叉
+    :param mating_pool: ndarray用于繁殖的个体的决策变量值，形状(noff,nvar)，即（要繁殖的个体数，决策变量数）
+    :param lb: ndarray，决策变量下界(lower bound)，形状(nvar,)
+    :param ub: ndarray，决策变量上界(upper bound)，形状(nvar,)
+    :param pc: float，交叉概率
+    :param eta_c: int，扩散因子分布指数，用于控制模拟二进制交叉的分布情况，值越大（>10），分布越均匀
+    :return: ndarray，交叉结果，形状为(noff, nvar)
+    """
     (noff, nvar) = mating_pool.shape
     nm = int(noff / 2)
-    parent1 = mating_pool[:nm]
+    parent1 = mating_pool[:nm]  #拆分
     parent2 = mating_pool[nm:]
     beta = np.zeros((nm, nvar))
     mu = np.random.random((nm, nvar))
@@ -135,16 +179,24 @@ def crossover(mating_pool, lb, ub, pc, eta_c):
     beta = beta * (-1) ** np.random.randint(0, 2, (nm, nvar))
     beta[np.random.random((nm, nvar)) < 0.5] = 1
     beta[np.tile(np.random.random((nm, 1)) > pc, (1, nvar))] = 1
-    offspring1 = (parent1 + parent2) / 2 + beta * (parent1 - parent2) / 2
+    offspring1 = (parent1 + parent2) / 2 + beta * (parent1 - parent2) / 2   # 交叉
     offspring2 = (parent1 + parent2) / 2 - beta * (parent1 - parent2) / 2
-    offspring = np.concatenate((offspring1, offspring2), axis=0)
+    offspring = np.concatenate((offspring1, offspring2), axis=0)    # 重新拼接
     offspring = np.min((offspring, np.tile(ub, (noff, 1))), axis=0)
     offspring = np.max((offspring, np.tile(lb, (noff, 1))), axis=0)
     return offspring
 
 
 def mutation(pop, lb, ub, pm, eta_m):
-    # polynomial mutation
+    """
+    polynomial mutation 多项式变异
+    :param pop: ndarray用于繁殖的个体的决策变量值，形状(noff,nvar)，即（要繁殖的个体数，决策变量数）
+    :param lb: ndarray，决策变量下界(lower bound)，形状(nvar,)
+    :param ub: ndarray，决策变量上界(upper bound)，形状(nvar,)
+    :param pm: float，变异概率
+    :param eta_m: 扰动因子分布指数，用于控制多项式变异的分布形状，值很大时，变异幅度较小，变异的形状更趋于均匀分布
+    :return: ndarray，交叉结果，形状为(noff, nvar)
+    """
     (npop, nvar) = pop.shape
     lb = np.tile(lb, (npop, 1))
     ub = np.tile(ub, (npop, 1))
@@ -162,7 +214,15 @@ def mutation(pop, lb, ub, pm, eta_m):
 
 
 def environmental_selection(pop, objs, zmin, npop, V):
-    # NSGA-III environmental selection
+    """
+    NSGA-III environmental selection
+    :param pop: ndarray，用于繁殖的个体的决策变量值，形状(noff,nvar)，即（要繁殖的个体数，决策变量数）
+    :param objs: ndarray，种群中每个个体的目标函数值，形状为 (npop, nobj)，nobj 是目标函数的数量。
+    :param zmin: ndarray，每个目标函数的最小值。它的形状为 (nobj,)
+    :param npop: int，环境选择后要保留的个体数量
+    :param V: 权向量，用于计算多目标优化中的 Pareto 前沿，形状通常是 (nv, nobj)，即（权向量的数量，目标函数的数量）每个权向量代表一种目标函数权重的组合
+    :return:
+    """
     pfs, rank = nd_sort(objs)
     nobj = objs.shape[1]
     selected = np.full(pop.shape[0], False)
@@ -239,8 +299,8 @@ def main(npop, iter, lb, ub, nobj=3, pc=1, pm=1, eta_c=30, eta_m=20):
     :param nobj: the dimension of objective space
     :param pc: crossover probability (default = 1)
     :param pm: mutation probability (default = 1)
-    :param eta_c: spread factor distribution index (default = 30)
-    :param eta_m: perturbance factor distribution index (default = 20)
+    :param eta_c: spread factor distribution index (default = 30) 扩散因子分布指数
+    :param eta_m: perturbance factor distribution index (default = 20) 扰动因子分布指数
     :return:
     """
     # Step 1. Initialization
